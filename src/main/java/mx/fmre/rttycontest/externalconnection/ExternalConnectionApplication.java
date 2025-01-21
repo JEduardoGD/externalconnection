7f9da5b52a45cb9be9ab3819015ffca87eaa5ce1
Eduardo Gutierrez <eduardo_gd@hotmail.com>
2025-01-21 07:59:05 -0600

    first commit

---
+++ src/main/java/mx/fmre/rttycontest/externalconnection/ExternalConnectionApplication.java
@@ -0,0 +1,13 @@
+package mx.fmre.rttycontest.externalconnection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExternalConnectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExternalConnectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/fmre/rttycontest/externalconnection/ExternalConnectionApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.fmre.rttycontest.externalconnection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExternalConnectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
