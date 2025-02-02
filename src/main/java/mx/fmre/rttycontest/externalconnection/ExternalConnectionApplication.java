9162ae4aae271ba3916c70213adc3f7b9949055e
Eduardo Gutierrez <eduardo_gd@hotmail.com>
2025-02-01 22:32:08 -0600

    correccion timezone y log

--- src/main/java/mx/fmre/rttycontest/externalconnection/ExternalConnectionApplication.java
+++ src/main/java/mx/fmre/rttycontest/externalconnection/ExternalConnectionApplication.java
@@ -1,5 +1,7 @@
 package mx.fmre.rttycontest.externalconnection;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ExternalConnectionApplication {
 
 	public static void main(String[] args) {
+    	TimeZone.setDefault(TimeZone.getTimeZone("America/Mexico_City"));
 		SpringApplication.run(ExternalConnectionApplication.class, args);
 	}
 
